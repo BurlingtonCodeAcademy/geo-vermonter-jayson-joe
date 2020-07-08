ee4aa5b2b283da8b104725928f28a1013111db3e
joemarcotte <61711467+joemarcotte@users.noreply.github.com>
2020-07-08 16:58:09 -0400

    list'

---
+++ src/VTMap.js
@@ -0,0 +1,3 @@
+import React from "react";
+import { Map, TileLayer, Polygon } from "react-leaflet";
+import borderData from "./border.js";
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import VTMap from './VTMap'
 
 ReactDOM.render(
-  <h1>Hello World!</h1>,
-  document.getElementById('root')
+  <div id="map"> 
+  <VTMap></VTMap>
+  </div>
 )
