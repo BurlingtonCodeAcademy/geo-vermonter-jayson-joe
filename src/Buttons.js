6fa9f740cf4d510b369528f40a626028d5a79104
Jayson1001 <64107977+Jayson1001@users.noreply.github.com>
2020-07-11 20:26:13 -0400

    Sat8pm

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { LeafletConsumer } from "react-leaflet";
+import VTMap from "./VTMap";
+import leafletPip from "@mapbox/leaflet-pip";
+import borderData from './border'
+import L from 'leaflet'
+import { Map } from "react-leaflet";
 
 class Buttons extends React.Component {
   constructor(props) {
@@ -7,38 +11,38 @@ class Buttons extends React.Component {
 
     this.state = {
       disabled: true,
+
     };
   }
 
-handleClick=(event) => {
-    event.preventDefault()
+  handleClick = (event) => {
+    event.preventDefault();
 
-    this.setState({disabled: false})
-    randomCoords()
-    leafletPip.pointInLayer(point, layer L.GeoJSON)
-}
+    this.setState({ disabled: false });
+    let startCoords = randomCoords();
+    this.setState({})
+    /*start() -----------this will be created to start the gameflow------------*/
+  };
 
-
-render() {
+  render() {
     return (
-        <div>
-            <button disabled={!this.state.disabled} onClick={this.handleClick}>START</button>
-            <button disabled={this.state.disabled}>GUESS</button>
-            <button disabled={this.state.disabled}>QUIT</button>
-        </div>
-    )
+      <div>
+        <button disabled={!this.state.disabled} onClick={this.handleClick}>
+          START
+        </button>
+        <button disabled={this.state.disabled}>GUESS</button>
+        <button disabled={this.state.disabled}>QUIT</button>
+      </div>
+    );
+  }
 }
-
-}
-
-
-
 
 function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
 }
 
 function randomCoords() {
+  let map = L.geoJSON(borderData)
   let latMin = 42.730315;
   let latMax = 45.005419;
 
@@ -48,6 +52,14 @@ function randomCoords() {
   let randomLat = getRandomArbitrary(latMin, latMax);
   let randomLong = getRandomArbitrary(longMin, longMax);
 
+  let pipArray = leafletPip.pointInLayer([randomLong, randomLat], map);
+  while (pipArray.length === 0) {
+      randomLat = getRandomArbitrary(latMin, latMax);
+      randomLong = getRandomArbitrary(longMin, longMax);
+    pipArray = leafletPip.pointInLayer([randomLong, randomLat], map);
+  }
+  
+ 
   return [randomLat, randomLong];
 }
 
