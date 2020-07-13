e041842a2b02d9e11cea70c5c8fed6f5d63d8fd9
joemarcotte <61711467+joemarcotte@users.noreply.github.com>
2020-07-13 09:52:31 -0400

    updated modals

--- src/Navigation.js
+++ src/Navigation.js
@@ -2,36 +2,21 @@ import React from "react";
 import leafletPip from "@mapbox/leaflet-pip";
 import borderData from "./border";
 import L from "leaflet";
-import Buttons from "./Buttons"
 
 class Navigation extends React.Component {
-  constructor(props) {
+    constructor(props) {
     super(props);
 
     this.state = {
       disabled: true,
-      gameStarted: false,
+      gameStarted: true,
+      direction: "",
     };
   }
 
-  handleClick = (event) => {
-    event.preventDefault();
-
-    this.setState({
-      disabled: false,
-      gameStarted: true
-    });
-  };
-
   render() {
-    return (
-    <div id="Navigation">
-      <button disabled={!this.props.disabled} onClick={this.props.handleClick}>North</button>
-      <button disabled={!this.props.disabled} onClick={this.props.handleClick}>South</button>
-      <button disabled={!this.props.disabled} onClick={this.props.handleClick}>East</button>
-      <button disabled={!this.props.disabled} onClick={this.props.handleClick}>West</button>
-    </div>
-    )}
+    return <div className="Navigation"></div>;
+  }
 }
 
 export default Navigation;
--- src/VTMap.js
+++ src/VTMap.js
@@ -8,70 +8,70 @@ import leafletPip from "@mapbox/leaflet-pip";
 import Navigation from "./Navigation";
 
 class VTMap extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      center: [44.0886, -72.7317],
-      zoom: 7,
-      style: { height: "600px", width: "600px" },
-      zoomControl: false,
-      scrollWheelZoom: false,
-      attributionControl: false,
-      disabled: true
-    };
-  }
-  handleClick = (event) => {
-    event.preventDefault();
-    let zoomCoords = randomCoords()
-    this.setState({
-      disabled: false,
-      gameStarted: true,
-      zoom: 18,
-      center: zoomCoords,
-    });
-  };
-  render() {
-    let VTBorder = borderData.geometry.coordinates[0].map((coordSet) => {
-      return [coordSet[1], coordSet[0]];
-    });
-    console.log(VTBorder);
+		this.state = {
+			center: [44.0886, -72.7317],
+			zoom: 7,
+			style: { height: "600px", width: "600px" },
+			zoomControl: false,
+			scrollWheelZoom: false,
+			attributionControl: false,
+			disabled: true,
+		};
+	}
+	handleClick = (event) => {
+		event.preventDefault();
+		let zoomCoords = randomCoords();
+		this.setState({
+			disabled: false,
+			gameStarted: true,
+			zoom: 18,
+			center: zoomCoords,
+		});
+	};
+	render() {
+		let VTBorder = borderData.geometry.coordinates[0].map((coordSet) => {
+			return [coordSet[1], coordSet[0]];
+		});
+		console.log(VTBorder);
 
-    return (
-      <>
-        <div>
-          <Map
-            center= {this.state.center}
-            zoom= {this.state.zoom}
-            style={this.state.style}
-            zoomControl={this.state.zoomControl}
-            scrollWheelZoom={this.state.scrollWheelZoom}
-            attributionControl={this.state.attributionControl}
-          >
-            <TileLayer
-              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-              attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
-            />
-            <Polygon positions={VTBorder} />
-          </Map>
-        </div>
-        <div>
-          <Buttons
-           disabled = {this.state.disabled}
-           handleClick = {this.handleClick}
-           guessButton = {this.state.guessButton}
-           quitButton = {this.state.quitButton}
-           />
-        </div>
-        <div>
-          <Navigation 
-          disabled = {this.state.disabled}
-          handleClick = {this.handleClick}
-          />
-        </div>
-      </>
-    );
-  }
+		return (
+			<>
+				<div>
+					<Map
+						center={this.state.center}
+						zoom={this.state.zoom}
+						style={this.state.style}
+						zoomControl={this.state.zoomControl}
+						scrollWheelZoom={this.state.scrollWheelZoom}
+						attributionControl={this.state.attributionControl}
+					>
+						<TileLayer
+							url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+							attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+						/>
+						<Polygon positions={VTBorder} />
+					</Map>
+				</div>
+				<div>
+					<Buttons
+						disabled={this.state.disabled}
+						handleClick={this.handleClick}
+						guessButton={this.state.guessButton}
+						quitButton={this.state.quitButton}
+					/>
+				</div>
+				<div>
+					<Navigation
+						disabled={this.state.disabled}
+						handleClick={this.handleClick}
+					/>
+				</div>
+			</>
+		);
+	}
 }
 function getRandomArbitrary(min, max) {
 	return Math.random() * (max - min) + min;
@@ -93,3 +93,4 @@ function randomCoords() {
 	return [randomLat, randomLong];
 }
 export default VTMap;
+//`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=geojson`
